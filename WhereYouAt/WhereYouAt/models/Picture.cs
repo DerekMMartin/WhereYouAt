f2eef30f5881b0b76d1a1c47e9a879de26813340
DerekMMartin <Derekmikelmartin@gmail.com>
2018-05-17 19:20:43 -0600

    Picture.cs and User.cs

---
+++ WhereYouAt/WhereYouAt/models/Picture.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace WhereYouAt.models
+{
+    public class Picture
+    {
+        public int ownerid { get; set; }
+        public string text { get; set; }
+        public ImageSource Image{ get; set; }
+    }
+
+}
---
+++ WhereYouAt/WhereYouAt/models/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WhereYouAt.models
+{
+    class User
+    {
+        public User(int Id, string userName)
+        {
+            id = Id;
+            username = userName;
+        }
+        public int id { get; }
+        public string username { get; }
+        public string firstname { get; set; }
+        public string lastname { get; set; }
+        public Picture profilepicture { get; set; }
+    }
+}
