e8caa9d6e2273c91deb98afd35dd88636b89af41
DerekMMartin <Derekmikelmartin@gmail.com>
2018-05-30 18:42:40 -0600

    Stories cleaned up. Now on creating can provide a byte[] for the profile picture. Add images with AddImage() and passing a byte[] through.

--- WhereYouAt/WhereYouAt/views/Story.xaml.cs
+++ WhereYouAt/WhereYouAt/views/Story.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,34 +15,39 @@ namespace WhereYouAt.views
 	{
         //spoofing for test remove when db working
         static int count = 0;
-		public Story ()
+		public Story (byte[] ProfilePicture)
         {//When Adding pictures set the HeightRequest = App.DisplayScreenHeight/(How many pictures per page. 5 is good for now.)
             InitializeComponent();
-            ProfileImage.Source = ImageSource.FromUri(new Uri(@"https://pbs.twimg.com/profile_images/891014228777226240/-_N7QxuJ_400x400.jpg"));
+            ProfileImage.Source = ImageSource.FromStream(() => new MemoryStream(ProfilePicture));
             ProfileImage.HeightRequest = 50;
             ProfileFrame.HeightRequest = 50;
-            for (int i = 0; i < 12; i++)
-            {
-                if (count == 0)
-                {
-                    Lay.Children.Add(new Image()
-                    { Source = ImageSource.FromUri(new Uri(@"https://assets-cdn.github.com/images/modules/open_graph/github-logo.png")), HeightRequest = App.DisplayScreenHeight / 5 });
-                }
-                else
-                {
-                    Lay.Children.Add(new Image() { Source = ImageSource.FromUri(new Uri(@"https://www.petri.com/wp-content/uploads/2015/11/Visual-Studio-Hero.jpg")), HeightRequest = App.DisplayScreenHeight / 5 });
-                }
 
-            }
-            count++;
-            if (count > 1)
-                count = 0;
+
+
+
+            //ProfileImage.Source = ImageSource.FromUri(new Uri(@"https://pbs.twimg.com/profile_images/891014228777226240/-_N7QxuJ_400x400.jpg"));
+            //for (int i = 0; i < 12; i++)
+            //{
+            //    if (count == 0)
+            //    {
+            //        Lay.Children.Add(new Image()
+            //        { Source = ImageSource.FromUri(new Uri(@"https://assets-cdn.github.com/images/modules/open_graph/github-logo.png")), HeightRequest = App.DisplayScreenHeight / 5 });
+            //    }
+            //    else
+            //    {
+            //        Lay.Children.Add(new Image() { Source = ImageSource.FromUri(new Uri(@"https://www.petri.com/wp-content/uploads/2015/11/Visual-Studio-Hero.jpg")), HeightRequest = App.DisplayScreenHeight / 5 });
+            //    }
+
+            //}
+            //count++;
+            //if (count > 1)
+            //    count = 0;
 
         }
 
-        public void AddPicture(ImageSource source)
+        public void AddPicture(byte[] image)
         {
-
+            Lay.Children.Add(new Image() { Source = ImageSource.FromStream(() => new MemoryStream(image)), HeightRequest = App.DisplayScreenHeight / 5 });
         }
     }
 }
